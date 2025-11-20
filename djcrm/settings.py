4d39c60c089c3afa55d922f5f604115bd8e2031f
samanta <samantak4820@gmail.com>
2025-11-20 14:12:31 +0545

    Modify styles

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(Path(BASE_DIR) / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
